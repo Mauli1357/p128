21754e3f5af26dc8375323f9f88c57406d01e129
Mauli1357 <71227691+Mauli1357@users.noreply.github.com>
2023-11-30 21:55:03 -0500

    Add files via upload

---
+++ brown_dwarfs_stars_data_scraping.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+# IMP NOTE: The page at the given URL is maintained by "wikipedia", which might be updated in future, hence the current data might be different.
+# Perform data scraping from scratch with HTML Tags/Class Names!
+
+
+# Task oneURLto Scrape Data
+url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+
+# Get url  using requests
+page = requests.get(url)
+
+
+# Parse Page
+soup = bs(page.content,'html.parser')
+
+# Get <table> with class = 'wikitable sortable'
+star_table = soup.find_all('table', {"class":"wikitable sortable"})
+
+total_table = len(star_table)
+
+
+temp_list= []
+
+
+table_rows = star_table[1].find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+#  creste empty list with the name of Stars_names=[],Distance=[],Mass=[],Radius=[]
+Star_names=[]
+Distance=[]
+Mass=[]
+Radius=[]
+
+print(temp_list)
+
+for i in range(1,len(temp_list)):
+    
+    Star_names.append(temp_list[i][0])
+    Distance.append(temp_list[i][5])
+    Mass.append(temp_list[i][7])
+    Radius.append(temp_list[i][8])
+
+# Convert to CSV
+headers = ['Star_name','Distance','Mass','Radius']  
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
+print(df2)
+
+# save it as dwarf_stars.csv
+df2.to_csv('new_scraped_daa.csv', index=True, index_label='id')
+
+
+
